f2dd0909f3f7bb641167da078a8717f95ede3d4f
Marcin Lasek <lasjan@tlen.pl>
2020-03-12 17:27:00 +0100

    Dodaj pliki projektów.

---
+++ PrimeTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
