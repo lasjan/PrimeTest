04aea6c147dfaf392424f48f23796da187b655c1
Marcin Lasek <lasjan@tlen.pl>
2020-03-12 19:59:53 +0100

    initial commit

--- PrimeTest/Program.cs
+++ PrimeTest/Program.cs
@@ -8,9 +8,20 @@ namespace PrimeTest
 {
     class Program
     {
+        static bool CheckIfPrime(int value)
+        {
+            for(int i = 0; i < value; i++)
+            {
+                if(i%value == 0 )
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
-            //testing
+            
         }
     }
 }
