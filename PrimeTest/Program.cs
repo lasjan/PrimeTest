0fb7efb3953074e18d94d8485180909b2473d288
Marcin Lasek <lasjan@tlen.pl>
2020-03-12 20:45:42 +0100

    final commit

--- PrimeTest/Program.cs
+++ PrimeTest/Program.cs
@@ -8,30 +8,6 @@ namespace PrimeTest
 {
     class Program
     {
-        static readonly string ERR_NOT_NUMBER = "Value '{0}' could not be parsed as integer..";
-        static readonly string ERR_START_HIGHER_THAN_END = "Start index '{0}' is lower than end index '{1}'";
-
-        static bool CheckIfPrime(int value)
-        {
-            for(int i = 2;  i < value; i++)
-            {
-                if(value % i == 0 )
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-        static void ProcessRange(int startIndex, int endIndex, Action<string> action)
-        {
-            for(int i = startIndex; i <= endIndex; i++)
-            {
-                if(CheckIfPrime(i))
-                {
-                    action(i.ToString());
-                }
-            }
-        }
         static void Main(string[] args)
         {
             try
@@ -43,24 +19,29 @@ namespace PrimeTest
                 Console.WriteLine("Please enter start index..");
 
                 var startInput = Console.ReadLine();
-                if (!Int32.TryParse(startInput, out startIndex))
-                {
-                    throw new ArgumentException(String.Format(ERR_NOT_NUMBER, startInput));
-                }
+                startIndex = ParseUtils.ParseArg(startInput);
+
                 Console.WriteLine("Please enter end index..");
-                var endtInput = Console.ReadLine();
-                if (!Int32.TryParse(endtInput, out endIndex))
+                
+                var endInput = Console.ReadLine();
+                endIndex = ParseUtils.ParseArg(endInput);
+
+                if (startIndex >=endIndex)
                 {
-                    throw new ArgumentException(String.Format(ERR_NOT_NUMBER, endIndex));
+                    throw new ArgumentException(String.Format(ParseUtils.ERR_START_HIGHER_THAN_END, startIndex, endIndex));
                 }
-                ProcessRange(startIndex, endIndex, (v) => { Console.WriteLine(v); });
+
+                Console.WriteLine(String.Format("Parsing range from {0} and {1}", startInput, endInput));
+
+                ParseUtils.ProcessRange(startIndex, endIndex, (v) => { Console.WriteLine(v); });
             }
             catch(ArgumentException aex)
             {
-
+                Console.WriteLine("Argument exception.." + aex.Message);
             }
             catch(Exception ex)
             {
+                Console.WriteLine("Unexpected exception.." + ex.Message);
 
             }
             Console.WriteLine("Press any key..");
